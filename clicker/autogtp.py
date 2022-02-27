9d43234b6953375fa0b06a6d08a68cec76f340be
jWang <m13121269577@163.com>
2022-02-27 12:22:42 -0500

    update coordinates

--- clicker/autogtp.py
+++ clicker/autogtp.py
@@ -4,6 +4,9 @@
 3. Double click the window to make sure it fits the screen
 4. Run this directly in VSCode
 5. After finishing the FILE_COUNT files, go to finder and move the finished files to another directory
+
+When measuring mouse position on the screen, run `pyautogui.position()` in terminal
+Use ctrl + ` to toggle focus between editor and terminal
 """
 import pyautogui
 import time
@@ -12,7 +15,7 @@ pyautogui.PAUSE = 1
 pyautogui.FAILSAFE = True
 
 # how many files to export
-FILE_COUNT = 50
+FILE_COUNT = 30
 
 for i in range(FILE_COUNT):
     # click on gtp window
@@ -34,28 +37,28 @@ for i in range(FILE_COUNT):
     # file
     pyautogui.click(x=168, y=14, duration=0.5)
     # export
-    pyautogui.moveTo(x=179, y=310, duration=0.5)
+    pyautogui.moveTo(x=179, y=350, duration=0.5)
     # audio
-    pyautogui.click(x=408, y=422, duration=0.1)
+    pyautogui.click(x=408, y=483, duration=0.1)
     # confirm export
-    pyautogui.click(x=880, y=492, duration=0.5)
+    pyautogui.click(x=880, y=654, duration=0.5)
 
     # change dir
-    pyautogui.click(x=738, y=247, duration=0.5)
-    pyautogui.click(x=738, y=270, duration=0.5)
+    pyautogui.click(x=738, y=255, duration=0.5)
+    pyautogui.click(x=738, y=282, duration=0.5)
 
     # double click somehow doesn't work, this is the alternative
-    pyautogui.click(x=738, y=304, duration=0.5)
+    pyautogui.click(x=738, y=334, duration=0.5)
     pyautogui.typewrite(["enter"])
 
     time.sleep(2)
 
     # click on save
-    pyautogui.moveTo(1118, 575, duration=0.5)
+    pyautogui.moveTo(1118, 548, duration=0.5)
     pyautogui.click()
 
     # uncheck "open dir when export is done"
-    pyautogui.click(x=593, y=479, duration=0.5)
+    pyautogui.click(x=598, y=489, duration=0.5)
 
     # wait for file to export
     print(f"{i+1} / {FILE_COUNT}")
@@ -63,4 +66,4 @@ for i in range(FILE_COUNT):
     time.sleep(26)
 
     # close the file, so that we don't end up with a thousand tabs in guitar pro
-    pyautogui.click(x=1407, y=108, duration=0.5)
+    pyautogui.click(x=1407, y=115, duration=0.5)
